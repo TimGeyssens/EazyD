a93b42288970022d293304fd974bbccf5001ac8b
Tim Geyssens <tg@umbraco.com>
2013-09-26 12:27:46 +0200

    Finishing touch

--- EazyD/Providers/AutoGenKey.cs
+++ EazyD/Providers/AutoGenKey.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using EazyD.Extensions;
 using EazyD.Interfaces;
@@ -14,7 +15,7 @@ namespace EazyD.Providers
             var val = value.ToCamelCase();
             if (val.Length > 50)
                 val = val.Substring(0, 50);
-            return val;
+            return Regex.Replace(val, @"[^A-Za-z0-9]+", "");
         }
     }
 }
